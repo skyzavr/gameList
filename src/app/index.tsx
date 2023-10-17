9e4b4f6ce3d8044136f15075db1f0db6ef7cdb27
nino <dankozero111@gmail.com>
2023-10-17 12:49:34 +0300

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import './styles/index.css';
+import { Header } from '@widgets/header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,8 @@
+import classes from './Header.module.css';
+export const Header = () => {
+  return (
+    <header>
+      <div className={classes.title}>Game List</div>
+    </header>
+  );
+};
