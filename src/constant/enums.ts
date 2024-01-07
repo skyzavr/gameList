456c4c0dba47dbf44c1f60c3244b70646041d489
nino <dankozero111@gmail.com>
2024-01-07 14:25:37 +0300

    [add]: params

---
+++ src/constant/enums.ts
@@ -0,0 +1,24 @@
+export enum GenreParams {
+  ALL = '',
+  ARPG = 'argp',
+  MMOARPG = 'mmoarpg',
+  STRATEGY = 'strategy',
+  MMORPG = 'mmorpg',
+  FIGHTING = 'fighting',
+  ACTION_RPG = 'action_rpg',
+  BATTLE_ROYALE = 'battle_royale',
+  MOBA = 'moba',
+  CARD = 'card',
+  SPORTS = 'sports',
+  RACING = 'racing',
+  CARD_GAME = 'card_game',
+  MMO = 'mmo',
+  SOCIAL = 'social',
+  FANTASY = 'fantasy',
+}
+export enum FilterParams {
+  TITLE = 'title',
+  DATE = 'release_date',
+  PLATFORM = 'platform',
+  DEV = 'developer',
+}
---
+++ src/constant/params.ts
@@ -0,0 +1,77 @@
+import { GenreParams, FilterParams } from './enums';
+
+export interface IOptions {
+  id: number;
+  name: string;
+  value: GenreParams | FilterParams;
+}
+
+export const FilterParam: IOptions[] = [
+  { id: 1, name: 'Title', value: FilterParams.TITLE },
+  { id: 2, name: 'Date Release', value: FilterParams.DATE },
+  { id: 3, name: 'Platform', value: FilterParams.PLATFORM },
+  { id: 4, name: 'Developer', value: FilterParams.DEV },
+];
+
+export const GenreParam: IOptions[] = [
+  { id: 1, name: 'All', value: GenreParams.ALL },
+  { id: 2, name: 'ARPG', value: GenreParams.ARPG },
+  { id: 3, name: 'MMOARPG', value: GenreParams.MMOARPG },
+  { id: 4, name: 'Strategy', value: GenreParams.STRATEGY },
+  { id: 5, name: 'MMORPG', value: GenreParams.MMORPG },
+  { id: 6, name: 'Fighting', value: GenreParams.FIGHTING },
+  { id: 7, name: 'Action RPG', value: GenreParams.ACTION_RPG },
+  { id: 8, name: 'Battle Royale', value: GenreParams.BATTLE_ROYALE },
+  { id: 9, name: 'MOBA', value: GenreParams.MOBA },
+  { id: 10, name: 'Card', value: GenreParams.CARD },
+  { id: 11, name: 'Sports', value: GenreParams.SPORTS },
+  { id: 12, name: 'Racing', value: GenreParams.RACING },
+  { id: 13, name: 'Card Game', value: GenreParams.CARD_GAME },
+  { id: 14, name: 'MMO', value: GenreParams.MMO },
+  { id: 15, name: 'Social', value: GenreParams.SOCIAL },
+  { id: 16, name: 'Fantasy', value: GenreParams.FANTASY },
+];
+
+export const API = 'https://www.freetogame.com/api/';
+
+export type Game = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  release_date: string;
+  short_description: string;
+};
+export type GameRS = {
+  os: string;
+  processor: string;
+  memory: string;
+  graphics: string;
+  storage: string;
+};
+export type Images = { id: number; image: string };
+export type GameDescT = {
+  title: string;
+  thumbnail: string;
+  description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  minimum_system_requirements: GameRS;
+  screenshots: Images[];
+};
+
+interface IData {
+  [key: string]: string;
+}
+export const SYMB_LEN = 250;
+export const options: IData = {
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+};
