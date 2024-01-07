f505bce27ed24cf5c21fc325d4cbdc22dd479f33
nino <dankozero111@gmail.com>
2024-01-07 14:18:11 +0300

    [fix]: colours update based on colour theme

--- src/pages/notFound/ui/NotFound.tsx
+++ src/pages/notFound/ui/NotFound.tsx
@@ -1,8 +1,9 @@
-import classes from './notFound.module.css';
-import { Icon } from '../assets/404';
-import Button from '@shared/ui/Button/Button';
-
 import { useNavigate } from 'react-router-dom';
+
+import Button from '@shared/ui/Button/Button';
+import { Icon } from '../assets/404';
+
+import classes from './notFound.module.css';
 
 export const NotFound = () => {
   const navigate = useNavigate();
@@ -16,7 +17,7 @@ export const NotFound = () => {
       <Icon />
       <div className={classes.title}>Oups</div>
       <div className={classes.desc}>There is error. Accept it and move</div>
-      <Button clickHandler={navigateToHome}>Time to move</Button>
+      <Button clickHandler={navigateToHome}>Back to List</Button>
     </section>
   );
 };
