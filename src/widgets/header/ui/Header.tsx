04d16b85904123c9ac2d9f7553124f8453a1d6d0
nino <dankozero111@gmail.com>
2024-01-07 14:10:17 +0300

    [add]: header component

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,8 +1,34 @@
-import classes from './Header.module.css';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { clearSearchQuery } from '@shared/ui/Search/modal/searchSlice';
+import { clearSortParam } from '@widgets/SettingArea/modal/sortSlice';
+import { clearFilterParam } from '@widgets/SettingArea/modal/filterSlice';
+
+import ThemeSwitcher from '@shared/ui/themeSwitcher/ThemeSwitcher';
+
+import classes from './header.module.css';
+
 export const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    dispatch(clearSearchQuery());
+    dispatch(clearSortParam());
+    dispatch(clearFilterParam());
+    navigate('/');
+  };
+
   return (
     <header>
-      <div className={classes.title}>Game List</div>
+      <div className={classes.topWrapper}>
+        <div className={classes.title} onClick={navigateToHome}>
+          Game List
+        </div>
+        <ThemeSwitcher />
+      </div>
+      <div className={classes.line}></div>
     </header>
   );
 };
