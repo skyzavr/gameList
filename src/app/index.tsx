5b7d8c3b5449d3709286b107ef33ca723f896a6c
nino <dankozero111@gmail.com>
2024-01-07 14:26:47 +0300

    [add]: routing, theme context

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,46 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { AppLayout } from '@pages/AppLayout';
+import { Home } from '@pages/home';
+import { GamePage } from '@pages/gamePage';
+import { NotFound } from '@pages/notFound';
+import ScrollTo from '@shared/ui/ScrollTo/ScrollTo';
+
+import ThemeContextProvider from './context/ThemeContext';
+
 import './styles/index.css';
-import { Header } from '@widgets/header';
+
+const dataMode = (paramName: string, preferedParam: string): string => {
+  const isParamExist = localStorage.getItem(paramName);
+  return isParamExist || preferedParam;
+};
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/gamePage',
+        element: <GamePage />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <>
-      <Header />
-    </>
+    <ThemeContextProvider init={dataMode('theme', 'dark')}>
+      <RouterProvider router={router} />
+      <ScrollTo />
+    </ThemeContextProvider>
   );
 };
 
